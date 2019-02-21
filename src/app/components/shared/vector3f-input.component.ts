21ffee45cd211b90dbbdf5794b94f68936deb6b7
Ralph Gasser <ralph.gasser@unibas.ch>
2019-02-21 11:58:01 +0100

    It is now possible to specify the Vecto3f components the Vector3fInputComponent should display.

--- src/app/components/shared/vector3f-input.component.ts
+++ src/app/components/shared/vector3f-input.component.ts
@@ -4,23 +4,36 @@ import {Vector3f} from '../../model/interfaces/general/vector-3f.model';
 @Component({
     selector: 'app-vector3f-input',
     template: `
-        <mat-form-field style="width: 33%;">
+        <mat-form-field style="width: 33%;" *ngIf="show[0]">
             <input matInput placeholder="{{name}} (x)" type="number" [(ngModel)]="vector.x">
         </mat-form-field>
-        <mat-form-field style="width: 33%;">
+        <mat-form-field style="width: 33%;" *ngIf="show[1]">
             <input matInput placeholder="{{name}} (y)" type="number" [(ngModel)]="vector.y">
         </mat-form-field>
-        <mat-form-field style="width: 33%;">
+        <mat-form-field style="width: 33%;" *ngIf="show[2]">
             <input matInput placeholder="{{name}} (z)" type="number" [(ngModel)]="vector.z">
         </mat-form-field>
     `
 })
 export class Vector3fInputComponent {
+    /** The {Vector3f} that is backing this {Vector3fInputComponent}. */
     @Input('vector')
     private _vector: Vector3f = null;
 
+    /** Name label for this {Vector3fInputComponent}. */
     @Input('name')
     private _name: string;
+
+    /** Determines which {Vector3f} components to show. */
+    @Input('show')
+    private _show: boolean[] = [true, true, true];
+
+    /**
+     * Getter for the show array.
+     */
+    get show() {
+        return this._show;
+    }
 
     /**
      * Getter for the name of the {Vector3fInputComponent}.
